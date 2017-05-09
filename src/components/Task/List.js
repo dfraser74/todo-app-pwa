f85ee51ea94dc93b10623c52ead9778e65b9db87
Tranxuanvu <xuanvu442@gmail.com>
2017-05-09 14:30:41 +0700

    add screen home

--- src/components/Task/List.js
+++ src/components/Task/List.js
@@ -3,8 +3,36 @@ import {List, ListItem} from 'material-ui/List';
 import Subheader from 'material-ui/Subheader';
 import Divider from 'material-ui/Divider';
 import { observer } from 'mobx-react';
+import Avatar from 'material-ui/Avatar';
+import FaIconClock from 'react-icons/lib/fa/clock-o';
+import MdLocationOn from 'react-icons/lib/md/location-on';
 
 import TaskService from '../../services/task';
+
+const styles = {
+  subheader: {
+    fontSize: 16,
+    paddingLeft: 0
+  },
+  containerList: {
+    flex: 1,
+    paddingLeft: 30,
+    paddingRight: 30
+  },
+  containerListItem: {
+    marginLeft: -16
+  },
+  nameItem: {
+    color: '#1D1D26',
+    fontSize: 20
+  },
+  timeLocation: {
+    display: 'flex',
+    alignItems: 'center',
+    opacity: 0.7,
+    padding: '3px 0'
+  }
+};
 
 @observer
 class Lists extends Component {
@@ -18,14 +46,16 @@ class Lists extends Component {
   }
 
   renderItemContent(task) {
-    const { description, category } = task;
+    const { description, category, timestart, timeend, location } = task;
 
     return (
       <div style={{height: 'initial'}}>
-        <div style={{display: 'flex'}}>{category}</div>
-        <div style={{marginTop: 20}}>
-          {description}
+        <div style={styles.timeLocation}>
+          <span style={{display: 'flex'}}><FaIconClock />&nbsp;{timestart} – {timeend} &nbsp;&nbsp;&nbsp;&nbsp;</span>
+          <span style={{display: 'flex'}}><MdLocationOn />&nbsp;{location}</span>
         </div>
+        <div style={{display: 'flex'}}>{category}</div>
+        <div style={{color: '#1D1D26', opacity: '0.8'}}>{description}</div>
       </div>
     );
   }
@@ -35,19 +65,24 @@ class Lists extends Component {
   }
 
   renderTask(task, key) {
-    const { name, description } = task;
-    const category = null
+    const { name, description, timestart, timeend, location } = task;
+    const category = null;
 
     return (
       [
         <ListItem
+          style={styles.containerListItem}
           key={key}
-          primaryText={name}
+          primaryText={
+            <span style={styles.nameItem}>{name}</span>
+          }
           secondaryTextLines={2}
           rightIconButton={this.renderItemRight(key)}
-          secondaryText={this.renderItemContent({ category, description })}
+          secondaryText={this.renderItemContent({ category, name, description, timestart, timeend, location })}
+
+          rightAvatar={<Avatar src="http://www.material-ui.com/images/ok-128.jpg" />}
         />,
-        <Divider />,
+        <Divider />
       ]
     );
   }
@@ -58,9 +93,9 @@ class Lists extends Component {
     const tasks = TaskService[subTitle] || {};
 
     return (
-      <List style={{flex: 1, margin: 10}}>
-        <Subheader>{subTitle}</Subheader>
-        <Divider />
+      <List style={styles.containerList}>
+        {/*<Subheader style={styles.subheader}>{subTitle}</Subheader>
+        <Divider />*/}
         {Object.keys(tasks).map((key) => this.renderTask(tasks[key], key))}
       </List>
     )
